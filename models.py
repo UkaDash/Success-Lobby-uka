aff5a84527810b9bd8912ce2f8ce4f2f0da2560c
Uka Dashdavaa <108192613+UkaDash@users.noreply.github.com>
2026-04-06 21:55:04 -0400

    Update models.py

--- models.py
+++ models.py
@@ -7,3 +7,14 @@ class User(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    # for profile used for matching
+    major = db.Column(db.String(100))
+    availability = db.Column(db.String(50))  
+    campus = db.Column(db.String(100))   
+    bio = db.Column(db.String(500))      
+
+class StudentCourse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    course_code = db.Column(db.String(20), nullable=False)
+    course_name = db.Column(db.String(100), nullable=False)
